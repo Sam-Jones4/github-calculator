291b60915283b7fe99ab3147f863bbd0465fc15e
Sam Jones <samjones@Sams-MacBook-Pro-21.local>
2025-02-21 00:13:57 -0700

    Implemented the calculator in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter command:");
+
+        Calculator calculator = new Calculator();
+
+        while (scanner.hasNext()) {
+            String command = scanner.next();
+            if (command.equals("exit")) break;
+
+            switch (command) {
+                case "add":
+                    System.out.println(calculator.add(scanner.nextInt(), scanner.nextInt()));
+                    break;
+                case "subtract":
+                    System.out.println(calculator.subtract(scanner.nextInt(), scanner.nextInt()));
+                    break;
+                case "multiply":
+                    System.out.println(calculator.multiply(scanner.nextInt(), scanner.nextInt()));
+                    break;
+                case "divide":
+                    System.out.println(calculator.divide(scanner.nextInt(), scanner.nextInt()));
+                    break;
+                case "fibonacci":
+                    System.out.println(calculator.fibonacciNumberFinder(scanner.nextInt()));
+                    break;
+                case "binary":
+                    System.out.println(calculator.intToBinaryNumber(scanner.nextInt()));
+                    break;
+                default:
+                    System.out.println("Unknown command");
+            }
+        }
+        scanner.close();
     }
 }
+
